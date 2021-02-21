fabf94a04bad83d45e5e3b9854b2b968fc99601d
Srinivas Nagesh Pammi <SRINIVAS.N.PAMMI@aexp.com>
2021-02-21 21:17:03 +0530

    this is for commiting directly from working directory using -a option for commit)

--- test.java
+++ test.java
@@ -2,9 +2,10 @@ package com.luv2code.springboot.demo.myCoolApp;
 
 public class test {
     public static void main(String[] args) {
-        int j = 10; /// changed from 0 to 10 
+        int j = 10; /// changed from 0 to 10
         for (int i=0; i<j; i++) {
-            System.out.println("print test to fix a bug");
+            System.out.println("print test to fix a bug ooo");
+            System.out.println("print test the change made");
         }
     }
 }
